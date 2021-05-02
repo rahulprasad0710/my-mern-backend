cc2816aec1eea30b9ce7d67814eff01a4a5c8c06
rahulprasad0710 <raulshah2021@hotmail.com>
2021-05-02 16:30:08 +0530

    mern start

--- server/index.js
+++ server/index.js
@@ -4,17 +4,9 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); //to get data from post request from client side
 
 const port = 5000;
-
-app.get("/hello", (req, res) => {
-    res.send("Hello hellos world");
-});
-app.post("/hello", (req, res) => {
-    console.log(req.body);
-    res.status(200).json({ msg: "dekha hai pehli baar" });
-});
 
 app.get("/", (req, res) => {
     res.send("Hellos World!");
