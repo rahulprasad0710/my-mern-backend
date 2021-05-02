fbc9e5923c5aacadf475be754d4099d95757c8de
rahulprasad0710 <raulshah2021@hotmail.com>
2021-05-02 11:38:57 +0530

    my sern start

--- server/index.js
+++ server/index.js
@@ -3,15 +3,21 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const port = 5000;
 
 app.get("/hello", (req, res) => {
-    res.send("Hello hello");
+    res.send("Hello hellos world");
+});
+app.post("/hello", (req, res) => {
+    console.log(req.body);
+    res.status(200).json({ msg: "dekha hai pehli baar" });
 });
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hellos World!");
 });
 
 app.listen(port, () => {
